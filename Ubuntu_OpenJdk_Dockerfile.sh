bdb7cb54c34dcb78355bab73959275b4faf9bf9b
Ravi Sharma <ping2ravi@gmail.com>
2020-04-23 13:03:00 +0530

    Addded ubuntu JDK 14/15

---
+++ Ubuntu_OpenJdk_Dockerfile.sh
@@ -0,0 +1,69 @@
+PARAM_UBUNTU_VERSION=$1
+PARAM_OPEN_JDK_VERSION=$2
+
+OIFS=$IFS
+IFS='.' read -ra JDK_VERSION_ARRAY <<< "$PARAM_JDK_VERSION"
+
+
+#JAVA_MAJOR_VERSION=${JDK_VERSION_ARRAY[0]}
+#JAVA_MINOR_VERSION=${JDK_VERSION_ARRAY[1]}
+#JAVA_BUILD_VERSION=${JDK_VERSION_ARRAY[2]}
+#JAVA_DOWNLOAD_ID=${JDK_VERSION_ARRAY[3]}
+
+
+echo "FROM ping2ravi/ubuntu:${PARAM_UBUNTU_VERSION}"
+echo ""
+echo ""
+
+echo "MAINTAINER Ravi Sharma"
+
+echo ""
+echo ""
+echo "# Ubuntu ${PARAM_UBUNTU_VERSION}"
+echo "# Java Version ${PARAM_OPEN_JDK_VERSION}"
+#echo "ENV JAVA_VERSION_MAJOR ${JAVA_MAJOR_VERSION}"
+#echo "ENV JAVA_VERSION_MINOR ${JAVA_MINOR_VERSION}"
+#echo "ENV JAVA_VERSION_BUILD ${JAVA_BUILD_VERSION}"
+#echo "ENV JAVA_PACKAGE_NAME ${JAVA_PACKAGE_NAME}"
+
+
+echo ""
+echo ""
+echo "ENV JAVA_BASE_DIR /opt/java"
+echo "ENV JAVA_DIR ${PARAM_OPEN_JDK_VERSION}"
+echo "ENV JAVA_HOME /opt/java/\${JAVA_DIR}"
+echo ""
+echo ""
+
+if [ $PARAM_OPEN_JDK_VERSION == "jdk-14.0.1" ]
+then
+echo "ENV JAVA_DOWNLOAD_URL https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_linux-x64_bin.tar.gz"
+else
+echo "ENV JAVA_DOWNLOAD_URL https://download.java.net/java/early_access/jdk15/19/GPL/open${PARAM_OPEN_JDK_VERSION}-15-ea+19_linux-x64_bin.tar.gz"
+fi
+
+
+
+echo ""
+echo ""
+#echo "RUN apk --update add curl ca-certificates tar"
+echo ""
+#echo "RUN mkdir -p  \$JAVA_HOME;curl -jksSLH \"Cookie: oraclelicense=accept-securebackup-cookie\" \$JAVA_DOWNLOAD_URL --output \${JAVA_BASE_DIR}/jdk.tar.gz; ls -lrt \${JAVA_BASE_DIR}"
+echo "RUN apt-get update -y; apt-get install -y curl"
+echo "RUN mkdir -p  \$JAVA_HOME;curl \$JAVA_DOWNLOAD_URL --output \${JAVA_BASE_DIR}/jdk.tar.gz; ls -lrt \${JAVA_BASE_DIR}"
+echo "RUN ls -lrt /opt/java/; ls -lrt \$JAVA_HOME"
+echo "RUN tar -xf \${JAVA_BASE_DIR}/jdk.tar.gz --directory \${JAVA_BASE_DIR};"
+echo "RUN ls -lrt \${JAVA_BASE_DIR} "
+echo "RUN ls -lrt \${JAVA_BASE_DIR}; ls -lrt \$JAVA_HOME; ls -lrth /var/cache/apk/; ls -lrt /tmp/"
+echo "RUN rm -f \${JAVA_BASE_DIR}/jdk.tar.gz;"
+echo "RUN rm -f \${JAVA_HOME}/*src.zip;rm -fR \${JAVA_HOME}/jre;rm -fR /tmp/* ; rm -fR /var/cache/apk/*;rm -fR $JAVA_HOME/legal"
+echo "RUN ls -lrt /opt/java/; ls -lrt \$JAVA_HOME"
+echo "RUN df -h; du -h -d 1 /opt/java/; echo check \$JAVA_HOME; du -h -d 1 \$JAVA_HOME; echo check temp; du -h -d 1 /tmp; du -h -d 1 /; ls -lrt \$JAVA_HOME"
+
+
+
+
+
+
+
+IFS=$OIFS
--- generate_all.sh
+++ generate_all.sh
@@ -1,4 +1,5 @@
 ALPINE_VERSIONS=(3.10 3.11)
+UBUNTU_VERSIONS=(20.04)
 JDK_VERSIONS=(8.181.13.96a7b8442fe848ef90c96a2fad6ed6d1 10.0.2.19aef61b38124481863b1413dce1855f)
 OPEN_JDK_VERSIONS=(jdk-14.0.1 jdk-15)
 
@@ -34,5 +35,19 @@ for ALPINE_VERSION in "${ALPINE_VERSIONS[@]}"
       done
    done
 
+for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"
+   do
+   for JDK_VERSION in "${OPEN_JDK_VERSIONS[@]}"
+      do
+#         OIFS=$IFS
+#         IFS='.' read -ra JDK_VERSION_ARRAY <<< "$JDK_VERSION"
+#         IFS=$OIFS
+
+         DIR=openjdk/${JDK_VERSION}/ubuntu_$UBUNTU_VERSION
+         echo "$DIR"
+         mkdir -p $DIR
+         ./Ubuntu_OpenJdk_Dockerfile.sh $UBUNTU_VERSION $JDK_VERSION  > $DIR/Dockerfile
+      done
+   done
 
 
