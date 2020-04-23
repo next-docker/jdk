caa470fb9bec6551b539a0bd3b4c66ec02f543f5
Ravi Sharma <ping2ravi@gmail.com>
2020-04-23 11:53:00 +0530

    Updated JDK

--- Alpine_OpenJdk_Dockerfile.sh
+++ Alpine_OpenJdk_Dockerfile.sh
@@ -35,8 +35,13 @@ echo "ENV JAVA_HOME /opt/java/\${JAVA_DIR}"
 echo ""
 echo ""
 
-
+if [ $PARAM_OPEN_JDK_VERSION == "jdk-14.0.1" ]
+then
+echo "ENV JAVA_DOWLOAD_URL https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_linux-x64_bin.tar.gz"
+else
 echo "ENV JAVA_DOWLOAD_URL https://download.java.net/java/early_access/alpine/10/binaries/open${PARAM_OPEN_JDK_VERSION}-ea+10_linux-x64-musl_bin.tar.gz"
+fi
+
 
 
 echo ""
